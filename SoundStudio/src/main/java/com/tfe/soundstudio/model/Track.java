9a4031466aff37fcde6129e9f7f4e69094abca2b
a4l4e4x4 <a4l4e4x4@a4l4e4x4-pc>
2018-07-30 14:44:07 +0200

    traks and traks objects with const, get, set

--- SoundStudio/src/main/java/com/tfe/soundstudio/model/Track.java
+++ SoundStudio/src/main/java/com/tfe/soundstudio/model/Track.java
@@ -25,5 +25,49 @@ public class Track {
 	
 	@Relationship (type = "IS_IN", direction=Relationship.INCOMING)
 	List<TrackObject> objectList;
+	
+	public Track() {}
+
+	public Track(Long id, Integer number, String name, List<TrackObject> objectList) {
+		super();
+		this.id = id;
+		this.number = number;
+		this.name = name;
+		this.objectList = objectList;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<TrackObject> getObjectList() {
+		return objectList;
+	}
+
+	public void setObjectList(List<TrackObject> objectList) {
+		this.objectList = objectList;
+	};
+	
+	
 
 }
--- SoundStudio/src/main/java/com/tfe/soundstudio/model/TrackObject.java
+++ SoundStudio/src/main/java/com/tfe/soundstudio/model/TrackObject.java
@@ -26,5 +26,58 @@ public class TrackObject {
 	
 	@Relationship (type = "IS_IN")
 	List<Track> trackList;
+	
+	public TrackObject() {}
+
+	public TrackObject(Long id, String starttime, String name, String wave, List<Track> trackList) {
+		super();
+		this.id = id;
+		this.starttime = starttime;
+		this.name = name;
+		this.wave = wave;
+		this.trackList = trackList;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWave() {
+		return wave;
+	}
+
+	public void setWave(String wave) {
+		this.wave = wave;
+	}
+
+	public List<Track> getTrackList() {
+		return trackList;
+	}
+
+	public void setTrackList(List<Track> trackList) {
+		this.trackList = trackList;
+	};
+	
+	
 
 }
