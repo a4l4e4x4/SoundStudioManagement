ae02f00893aae20add279ea56efed68cc684c75a
a4l4e4x4 <a4l4e4x4@a4l4e4x4-pc>
2018-08-13 00:00:24 +0200

    Addition Controller and add pages + @{/path} to hrefs and scr

--- SoundStudio/src/main/java/com/tfe/soundstudio/controller/AdditionsController.java
+++ SoundStudio/src/main/java/com/tfe/soundstudio/controller/AdditionsController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tfe.soundstudio.model.Client;
 import com.tfe.soundstudio.model.Engineer;
+import com.tfe.soundstudio.model.Instrument;
 import com.tfe.soundstudio.model.Musician;
+import com.tfe.soundstudio.model.Piece;
+import com.tfe.soundstudio.model.Project;
 import com.tfe.soundstudio.service.ClientService;
 import com.tfe.soundstudio.service.EngineerService;
 import com.tfe.soundstudio.service.InstrumentService;
@@ -57,7 +60,7 @@ public class AdditionsController {
 	
 	@PostMapping(value="add/addClient")
 	public String addClientPost(@Valid Client client, BindingResult result, Model model) {
-		
+		clientService.saveClient(client);
 		return "clientList";
 	}
 	
@@ -71,7 +74,7 @@ public class AdditionsController {
 	
 	@PostMapping(value="add/addMusician")
 	public String addMusicianPost(@Valid Musician musician, BindingResult result, Model model) {
-		
+		musicianService.saveMusician(musician);
 		return "musicianList";
 	}
 	
@@ -85,8 +88,50 @@ public class AdditionsController {
 	
 	@PostMapping(value="add/addEngineer")
 	public String addEngineerPost(@Valid Engineer engineer, BindingResult result, Model model) {
-		
+		engineerService.saveEngineer(engineer);
 		return "engineerList";
+	}
+	
+	@GetMapping(value="add/addProject")
+	public String addProjectGet(Model model) {
+		
+		model.addAttribute("project", new Project());
+		
+		return "add/addProject";
+	}
+	
+	@PostMapping(value="add/addProject")
+	public String addProjectPost(@Valid Project project, BindingResult result, Model model) {
+		projectService.saveProject(project);
+		return "projectList";
+	}
+	
+	@GetMapping(value="add/addPiece")
+	public String addPieceGet(Model model) {
+		
+		model.addAttribute("piece", new Piece());
+		
+		return "add/addPiece";
+	}
+	
+	@PostMapping(value="add/addPiece")
+	public String addPiecePost(@Valid Piece piece, BindingResult result, Model model) {
+		pieceService.savePiece(piece);
+		return "pieceList";
+	}
+	
+	@GetMapping(value="add/addInstrument")
+	public String addInstrumentGet(Model model) {
+		
+		model.addAttribute("instrument", new Instrument());
+		
+		return "add/addInstrument";
+	}
+	
+	@PostMapping(value="add/addInstrument")
+	public String addInstrumentPost(@Valid Instrument instrument, BindingResult result, Model model) {
+		instrumentService.saveInstrument(instrument);
+		return "instrumentList";
 	}
 
 }
